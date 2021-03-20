f85475a5a0d0fdc9f34647405a8357f02156afc0
Tom Gilder <tom@tom.me.uk>
2021-03-20 23:46:52 +0200

    Add test

--- test/helpers.dart
+++ test/helpers.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 const kTransitionDuration = Duration(milliseconds: 310);
+
+/// Records changes in URL
+Future<List<String>> recordUrlChanges(Future Function() callback) async {
+  final result = <String>[];
+  SystemChannels.navigation.setMockMethodCallHandler((call) async {
+    if (call.method == 'routeInformationUpdated') {
+      result.add(call.arguments['location'] as String);
+    }
+  });
+
+  await callback();
+  SystemChannels.navigation.setMockMethodCallHandler(null);
+  return result;
+}
+
+/// Simulates pressing the system back button
+Future<void> invokeSystemBack() {
+  // ignore: invalid_use_of_protected_member
+  return WidgetsBinding.instance!.handlePopRoute();
+}
 
 class PageOne extends StatelessWidget {
   @override
--- test/unknown_route_test.dart
+++ test/unknown_route_test.dart
@@ -47,11 +47,48 @@ void main() {
       ),
     );
 
-    delegate.push('/unknown/nonsense');
-    await tester.pump();
-    await tester.pump(Duration(seconds: 1));
+    expect(
+      await recordUrlChanges(() async {
+        delegate.push('/unknown/nonsense');
+        await tester.pump();
+        await tester.pump(Duration(seconds: 1));
 
-    expect(find.byType(NotFoundPage), findsOneWidget);
+        expect(find.byType(NotFoundPage), findsOneWidget);
+      }),
+      ['/unknown/nonsense'],
+    );
+  });
+
+  testWidgets('Can redirect to 404 page', (tester) async {
+    final delegate = Routemaster(
+      routesBuilder: (_) => RouteMap(
+        onUnknownRoute: (rm, __, ___) {
+          rm.replace('/not-found');
+        },
+        routes: {
+          '/': (_) => MaterialPage<void>(child: PageOne()),
+          '/not-found': (_) => MaterialPage<void>(child: NotFoundPage()),
+        },
+      ),
+    );
+
+    await tester.pumpWidget(
+      MaterialApp.router(
+        routeInformationParser: RoutemasterParser(),
+        routerDelegate: delegate,
+      ),
+    );
+
+    expect(
+      await recordUrlChanges(() async {
+        delegate.push('/unknown/nonsense');
+        await tester.pump();
+        await tester.pump(Duration(seconds: 1));
+
+        expect(find.byType(NotFoundPage), findsOneWidget);
+      }),
+      ['/not-found'],
+    );
   });
 }
 
