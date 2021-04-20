73adc5caa81794f84eb4e244331634f667b72294
Tom Gilder <tom@gilder.dev>
2021-04-20 08:58:49 +0300

    Make tests run on Chrome

---
+++ test/stack_test_replace_vm.dart
@@ -0,0 +1,65 @@
+@TestOn('dart-vm')
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:routemaster/routemaster.dart';
+import 'helpers.dart';
+
+// These tests don't work if run from Chrome, as they use the browser's
+// native navigation functions.
+
+void main() {
+  testWidgets('Can push a page via replace()', (tester) async {
+    final delegate = RoutemasterDelegate(routesBuilder: (context) {
+      return RouteMap(routes: {
+        '/': (_) => MaterialPage<void>(child: PageOne()),
+        '/two': (_) => MaterialPage<void>(child: PageTwo()),
+      });
+    });
+
+    await tester.pumpWidget(
+      MaterialApp.router(
+        routeInformationParser: RoutemasterParser(),
+        routerDelegate: delegate,
+      ),
+    );
+
+    expect(delegate.currentConfiguration, RouteData('/'));
+    expect(find.byType(PageOne), findsOneWidget);
+    expect(find.byType(PageTwo), findsNothing);
+
+    delegate.replace('two');
+    await tester.pump();
+    await tester.pump(kTransitionDuration);
+
+    expect(delegate.currentConfiguration, RouteData('/two'));
+    expect(find.byType(PageOne), findsNothing);
+    expect(find.byType(PageTwo), findsOneWidget);
+  });
+
+  testWidgets('Can replace via Routemaster.of()', (tester) async {
+    final page1Key = GlobalKey();
+
+    await tester.pumpWidget(
+      MaterialApp.router(
+        routeInformationParser: RoutemasterParser(),
+        routerDelegate: RoutemasterDelegate(routesBuilder: (context) {
+          return RouteMap(routes: {
+            '/': (_) => MaterialPage<void>(child: PageOne(key: page1Key)),
+            '/two': (_) => MaterialPage<void>(child: PageTwo()),
+          });
+        }),
+      ),
+    );
+
+    expect(find.byType(PageOne), findsOneWidget);
+    expect(find.byType(PageTwo), findsNothing);
+
+    final routemaster = Routemaster.of(page1Key.currentContext!);
+    routemaster.replace('two');
+    await tester.pump();
+    await tester.pump(kTransitionDuration);
+
+    expect(find.byType(PageOne), findsNothing);
+    expect(find.byType(PageTwo), findsOneWidget);
+  });
+}
--- test/system_nav_vm_test.dart
+++ test/system_nav_vm_test.dart
@@ -1,7 +1,7 @@
+@TestOn('dart-vm')
 import 'package:flutter_test/flutter_test.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:routemaster/src/system_nav.dart';
-import 'package:routemaster/src/system_nav_main.dart';
 
 void main() {
   test('SystemNav.pathStrategy throws when not on web', () {
@@ -24,37 +24,5 @@ void main() {
 
   test('Routemaster.setPathUrlStrategy() does nothing when not on web', () {
     Routemaster.setPathUrlStrategy();
-  });
-
-  test('isReplacement returns correct values', () {
-    expect(isReplacementNavigation('blah'), isFalse);
-
-    expect(isReplacementNavigation({'state': null}), isFalse);
-
-    expect(
-      isReplacementNavigation({'state': null}),
-      isFalse,
-    );
-
-    expect(
-      isReplacementNavigation({
-        'state': {'isReplacement': null}
-      }),
-      isFalse,
-    );
-
-    expect(
-      isReplacementNavigation({
-        'state': {'isReplacement': false}
-      }),
-      isFalse,
-    );
-
-    expect(
-      isReplacementNavigation({
-        'state': {'isReplacement': true}
-      }),
-      isTrue,
-    );
   });
 }
