a3c4decfb64b1cfb4dc507ca41987fbea66663dc
Tom Gilder <tom@gilder.dev>
2021-04-29 16:49:11 +0300

    Fix build on Flutter master (#57)

--- lib/src/system_nav_web.dart
+++ lib/src/system_nav_web.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'system_nav.dart';
 
@@ -21,21 +22,32 @@ class SystemNav {
       "Note: using path URL strategy with Routemaster is experimental, and there's a high chance of bugs.\n\n"
       'Please file an issue at https://github.com/tomgilder/routemaster/issues if you have problems.',
     );
-    pathStrategy = PathStrategy.path;
+    _pathStrategy = PathStrategy.path;
     setUrlStrategy(RoutemasterPathUrlStrategy());
   }
 
-  static PathStrategy pathStrategy = PathStrategy.hash;
+  /// Used from tests: pretends we're using the path URL strategy.
+  /// Otherwise calls to replace() won't work from tests.
+  @visibleForTesting
+  static void setFakePathUrlStrategy() {
+    _pathStrategy = PathStrategy.path;
+  }
+
+  static PathStrategy _pathStrategy = PathStrategy.hash;
+  static PathStrategy get pathStrategy => _pathStrategy;
 }
 
 /// A custom URL strategy which supports replacing URLs.
 class RoutemasterPathUrlStrategy extends PathUrlStrategy {
   @override
-  void pushState(Object state, String title, String url) {
-    if (isReplacementNavigation(state)) {
-      replaceState(state, title, url);
-    } else {
+  // Uses dynamic to be compatible with different Flutter versions
+  void pushState(dynamic state, String title, String url) {
+    if (state != null && isReplacementNavigation(state)) {
+      replaceState(state as Object, title, url);
+    } else if (state is Object) {
       super.pushState(state, title, url);
+    } else {
+      super.pushState(Object(), title, url);
     }
   }
 }
