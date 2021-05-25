3145b1eb3d035ac3e46e1f9a5817fa0a523396eb
Tom Gilder <tom@gilder.dev>
2021-05-25 17:23:09 +0300

    Fix exception by setting primaryColor
    Closes #115

--- example/mobile_app/lib/main.dart
+++ example/mobile_app/lib/main.dart
@@ -23,7 +23,10 @@ class TitleObserver extends RoutemasterObserver {
   void didChangeRoute(RouteData routeData, Page page) {
     if (page.name != null) {
       SystemChrome.setApplicationSwitcherDescription(
-        ApplicationSwitcherDescription(label: page.name),
+        ApplicationSwitcherDescription(
+          label: page.name,
+          primaryColor: 0xFF00FF00,
+        ),
       );
     }
   }
