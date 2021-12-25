35876d033577263a2d3dc8b9b637032d8639684a
Tom Gilder <tom@gilder.dev>
2021-12-25 14:36:38 +0200

    Fix warnings from Flutter upgrade (#245)
    * Fix warnings from Flutter upgrade
    * Upgrade iOS projects

--- test/web_nav_hash_test.dart
+++ test/web_nav_hash_test.dart
@@ -1,5 +1,4 @@
 @TestOn('browser')
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:routemaster/routemaster.dart';
